e89a5c5ba7a7028f25ad2e1587874cc3e84038ec
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-12-09 19:31:28 +0200

    add problem Start or end of a word

---
+++ Problems/Start or end of a word/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String part = scanner.nextLine();
+        String line = scanner.nextLine();
+
+        Pattern p = Pattern.compile(part + "\\b.*|.*\\b" + part, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(line);
+        if (m.find()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
