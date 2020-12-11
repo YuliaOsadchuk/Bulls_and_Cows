d23ee23e8d34dc66ae9588df9feb2d989d15d7eb
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-12-11 23:43:00 +0200

    start problem Find all passwords

---
+++ Problems/Find all passwords/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern p = Pattern.compile("\\bpassword.+?\\S\\b", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(text);
+        boolean find = false;
+        while (m.find()) {
+            Pattern pp = Pattern.compile("password:?", Pattern.CASE_INSENSITIVE);
+
+            String s = m.group().replaceAll("password:?".toLowerCase(), "");
+            System.out.println(s.trim());
+            find = true;
+        }
+        if (!find) {
+            System.out.println("No passwords found.");
+        }
+    }
+}
