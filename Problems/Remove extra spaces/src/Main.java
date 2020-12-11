172e531b501a938ede5fbe5c34589dd0acc75a71
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-12-11 15:44:31 +0200

    add problem Remove extra spaces

---
+++ Problems/Remove extra spaces/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class RemoveExtraSpacesProblem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        text = text.replaceAll("\\s+", " ");
+        System.out.println(text);
+    }
+}
