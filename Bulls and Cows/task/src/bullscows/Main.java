a3c28a21868e5735b81f9b5b4f73787afc785b39
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-12-31 10:55:49 +0200

    add Stage 2/7: Grader and start Stage 3/7: Secret code

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,16 +1,58 @@
 package bullscows;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static int bulls = 0;
+    public static int cows = 0;
+    public static int secretNumber = 9305;
+
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.");
-        System.out.println();
-        System.out.println("Turn 1. Answer:");
-        System.out.println("1234");
-        System.out.println("Grade: None.");
-        System.out.println();
-        System.out.println("Turn 2. Answer:");
-        System.out.println("9876");
-        System.out.println("Grade: 4 bulls.");
-        System.out.println("Congrats! The secret code is 9876.");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        Random random = new Random();
+        //secretNumber = random.nextInt(10000);
+        scoreBulls(inputNumber);
     }
+
+    public static void scoreBulls(int inputNumber) {
+        if (inputNumber == secretNumber) {
+            bulls = 4;
+            printResult();
+            return;
+        }
+        String input = String.valueOf(inputNumber);
+        String secret = String.valueOf(secretNumber);
+        for (int i = 0; i < 4; i++) {
+            if (input.charAt(i) == secret.charAt(i)) {
+                bulls++;
+            }
+        }
+        scoreCows(inputNumber);
+    }
+
+    public static void scoreCows(int inputNumber) {
+        String input = String.valueOf(inputNumber);
+        String secret = String.valueOf(secretNumber);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (input.charAt(i) == secret.charAt(j) && i != j) {
+                    cows++;
+                    break;
+                }
+            }
+        }
+
+        printResult();
+    }
+
+    public static void printResult() {
+        if (bulls == 0 && cows == 0) {
+            System.out.println("Grade: None.  The secret code is " + secretNumber + ".");
+        } else {
+            System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretNumber + ".");
+        }
+    }
+
 }
